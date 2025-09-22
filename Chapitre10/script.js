af87985d0340322d5a3059ccbae61694cd8754e8
Zakariya Boutlane <zakariyaboulane1@gmail.com>
2025-09-22 11:39:23 +0100

    Add files via upload

---
+++ Chapitre10/script.js
@@ -0,0 +1,9 @@
+let age = 20;
+
+if (age < 18) {
+  console.log("Vous êtes mineur.");
+} else if (age === 18) {
+  console.log("Vous venez d atteindre la majorité !");
+} else {
+  console.log("Vous êtes majeur.");
+}
